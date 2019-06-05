e467f19feb64abb8409c63975d29e0f2a77a8c74
志村隼斗 <daken@shimurahayatonoMacBook-Air.local>
2019-06-05 12:30:08 +0900

    ログイン機能追加

--- ec_test/app/controllers/application_controller.rb
+++ ec_test/app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+
+	before_action :configure_permitted_parameters, if: :devise_controller?
+
+	protected
+
+	def configure_permitted_parameters
+		devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :email])
+		devise_parameter_sanitizer.permit(:sign_in, keys: [:last_name, :first_name, :email])
+		devise_parameter_sanitizer.permit(:account_update, keys: [:last_name, :first_name, :email])
+	end
 end
